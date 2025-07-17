76e1e252e8f96f98599214758fb1521dd00d72b9
iprernagarg <gargprerna56@gmail.com>
2025-07-17 16:49:03 +0530

    Initial commit with helloworld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip>
+//#include "this.h"- This will give an error if this.h file not present in the directory.
+using namespace std;
+// int difference=80;
+int main() {
+//     // int sum=6;
+//     // float pi=3.14;
+//     // cout <</*This is an insertion operator.*/ pi<< "\n";
+//     // cout << "Hello World\n";
+//     // cout <<sum;
+//     int num1,num2,sum,difference;
+//     cout<< "Enter the value of num1:\n";
+//     cin>> num1;
+//      cout<< "Enter the value of num2:\n";
+//     cin>> num2;
+// sum= num1+num2;
+// difference=num1-num2;
+// cout<< "The sum of the numbers is: "<<::difference;   //:: scope resolution operator.
+//*******************************************
+34.4; // by default this is double.
+34.4f; // f indicates floating point literal.
+//*******************************************
+// reference variable
+// float x=10;
+// float &y = x;
+// cout<< "The value of x and y is: "<<x&&y;
+//*******************************************
+// typecasting 
+// float o= 46.99;
+// cout<< "The value of o is: "<<(int)o<<"\n";
+// cout<< "The value of o is: "<<int(o);
+// ***************
+// MANIPULATORS
+int a=2,b=25,c=12360;
+// cout<<"The value of a without setw is:"<<a<<endl;
+// cout<<"The value of b without setw is:"<<b<<endl;
+// cout<<"The value of c without setw is:"<<c<<endl;
+// cout<<"The value of a wit setw is:"<<setw(5)<<a<<endl;
+// cout<<"The value of b with setw is:"<<setw(5)<<b<<endl;
+// cout<<"The value of c with setw is:"<<setw(5)<<c<<endl;
+
+
+
+
+    return 0;
+}
+
